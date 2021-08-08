adb04fc24b41fc9c9991dac70e9b736a59e6e541
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2021-08-08 12:28:19 +1200

    Got navigation working

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cool-Wheely-App
+This Sample App is designed to be an opportunity to demonstrate your understanding of the  different elements of App Development that are required day to day. 
+
+The app contains two screens: Add Text screen which leads to a Spin the wheel type screen. 
---
+++ app/src/main/java/com/mayurdw/wheelspinnerapp/fragments/AddItemsFragment.kt
@@ -0,0 +1,35 @@
+package com.mayurdw.wheelspinnerapp.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.mayurdw.wheelspinnerapp.R
+import com.mayurdw.wheelspinnerapp.databinding.AddItemsFragmentBinding
+import com.mayurdw.wheelspinnerapp.viewmodels.AddItemsViewModel
+
+class AddItemsFragment : Fragment() {
+
+    private lateinit var viewModel: AddItemsViewModel
+    private lateinit var binding: AddItemsFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.add_items_fragment, container, false)
+        viewModel = AddItemsViewModel()
+
+        binding.button.setOnClickListener {
+            view: View ->
+                view.findNavController().navigate(R.id.action_addItemsFragment_to_wheelSpinnerFragment)
+        }
+
+        return binding.root
+    }
+
+}

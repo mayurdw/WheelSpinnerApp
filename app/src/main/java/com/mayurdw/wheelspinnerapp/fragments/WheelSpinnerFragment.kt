4918d305d62cff7b96069a2adc852e5eff2bf7a7
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2021-08-09 21:03:23 +1200

    Got a normal wheel working. TODO: 1. Setup checks in Add Items fragment 2. Change Colors of Wheel to reflect the theme

--- app/src/main/java/com/mayurdw/wheelspinnerapp/fragments/WheelSpinnerFragment.kt
+++ app/src/main/java/com/mayurdw/wheelspinnerapp/fragments/WheelSpinnerFragment.kt
@@ -9,32 +9,54 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.navArgs
 import com.mayurdw.wheelspinnerapp.R
 import com.mayurdw.wheelspinnerapp.databinding.WheelSpinnerFragmentBinding
+import rubikstudio.library.LuckyWheelView
+import rubikstudio.library.model.LuckyItem
+import java.util.*
+
 
 class WheelSpinnerFragment : Fragment() {
+    private val data = mutableListOf<LuckyItem>()
+    private val colors = arrayOf( 0xffFFE0B2.toInt(), 0xffFFCC80.toInt() )
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val listSize : Int
-
         val binding: WheelSpinnerFragmentBinding = DataBindingUtil.inflate(
             inflater, R.layout.wheel_spinner_fragment, container, false
         )
 
         val scoreFragmentArgs by navArgs<WheelSpinnerFragmentArgs>()
-
-
-        binding.wheelView.titles = scoreFragmentArgs.args.toList()
-        listSize = scoreFragmentArgs.args.toList().size
+        val luckyWheelView: LuckyWheelView = binding.luckyWheel
 
         binding.spinButton.setOnClickListener {
-            var newIndex = ( 0 until listSize).shuffled().last()
-            while( newIndex == binding.wheelView.focusedIndex ){
-                newIndex = ( 0 until listSize ).shuffled().last()
-            }
-            binding.wheelView.focusedIndex = newIndex
+            val index = getRandomIndex()
+            luckyWheelView.startLuckyWheelWithTargetIndex(index)
         }
+
+        this.populateListItems( scoreFragmentArgs.args.toList() )
+        luckyWheelView.setData( this.data )
+
         return binding.root
+    }
+
+    private fun populateListItems( stringList: List<String> ) {
+        for ( i in stringList.indices) run {
+            val luckyItem = LuckyItem()
+            luckyItem.secondaryText = stringList[i]
+            luckyItem.color = this.colors[ if ( i / 2 == 0 ) 0 else 1 ]
+            this.data.add(luckyItem)
+        }
+    }
+
+    private fun getRandomIndex(): Int {
+        val rand = Random()
+        return rand.nextInt( this.data.size - 1) + 0
+    }
+
+    private fun getRandomRound(): Int {
+        val rand = Random()
+        return rand.nextInt(10) + 15
     }
 
 

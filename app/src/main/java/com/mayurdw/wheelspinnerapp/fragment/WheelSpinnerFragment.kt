353b85b8263e19b3dde5011025143c812c81480e
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2021-10-11 21:32:57 +1300

    Wheel spinner added. Need to cater to viewModel & logic next

--- app/src/main/java/com/mayurdw/wheelspinnerapp/fragment/WheelSpinnerFragment.kt
+++ app/src/main/java/com/mayurdw/wheelspinnerapp/fragment/WheelSpinnerFragment.kt
@@ -1,33 +1,60 @@
 package com.mayurdw.wheelspinnerapp.fragment
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.navArgs
 import com.mayurdw.wheelspinnerapp.R
-import com.mayurdw.wheelspinnerapp.viewmodels.WheelSpinnerViewModel
+import com.mayurdw.wheelspinnerapp.databinding.WheelSpinnerFragmentBinding
+import rubikstudio.library.LuckyWheelView
+import rubikstudio.library.model.LuckyItem
+import java.util.*
 
 class WheelSpinnerFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = WheelSpinnerFragment()
-    }
-
-    private lateinit var viewModel: WheelSpinnerViewModel
+    private val data = mutableListOf<LuckyItem>()
+    private val colors = arrayOf(0xffFFE0B2.toInt(), 0xffFFCC80.toInt())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.wheel_spinner_fragment, container, false)
+    ): View {
+        val binding: WheelSpinnerFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.wheel_spinner_fragment, container, false
+        )
+
+        val scoreFragmentArgs by navArgs<WheelSpinnerFragmentArgs>()
+        val luckyWheelView: LuckyWheelView = binding.luckyWheel
+
+        binding.spinButton.setOnClickListener {
+            val index = getRandomIndex()
+            luckyWheelView.startLuckyWheelWithTargetIndex(index)
+        }
+
+        this.populateListItems(scoreFragmentArgs.itemLists.toList())
+        luckyWheelView.setData(this.data)
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(WheelSpinnerViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun populateListItems(stringList: List<String>) {
+        for (i in stringList.indices) run {
+            val luckyItem = LuckyItem()
+            luckyItem.secondaryText = stringList[i]
+            luckyItem.color = this.colors[if (i / 2 == 0) 0 else 1]
+            this.data.add(luckyItem)
+        }
     }
 
+    private fun getRandomIndex(): Int {
+        val rand = Random()
+        return rand.nextInt(this.data.size - 1) + 0
+    }
+//
+//    private fun getRandomRound(): Int {
+//        val rand = Random()
+//        return rand.nextInt(10) + 15
+//    }
 }
